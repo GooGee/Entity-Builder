2fb3842d12db2e0b448e5bcdecbf684e82676035
GooGee <mou-gj@live.com>
2020-04-08 09:27:24 +0800

    + router

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router'
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import App from './App.vue'
+import router from './router.js'
 
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
 
+Vue.use(VueRouter)
+
 Vue.config.productionTip = false
 
 new Vue({
+    router,
     render: (h) => h(App),
 }).$mount('#app')
