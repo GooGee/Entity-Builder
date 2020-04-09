80a93f96dc297a92b28dfe1222da0fbc65f6cadf
GooGee <mou-gj@live.com>
2020-04-09 14:09:35 +0800

    + SideBar

---
+++ src/components/SideBar.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h2>{{ sidebar.title }}
+            <b-button v-if="sidebar.manager" @click="add" variant="outline-primary"> + </b-button>
+        </h2>
+        <b-list-group v-if="sidebar.manager">
+            <b-list-group-item
+                v-for="item in sidebar.manager.list"
+                :key="item.name"
+                @click="sidebar.item = item"
+                :variant="Object.is(sidebar.item, item) ? 'primary' : ''"
+                button
+            >
+                {{ item.name }}
+            </b-list-group-item>
+        </b-list-group>
+    </div>
+</template>
+
+<script>
+import sidebar from '../states/sidebar.js'
+
+export default {
+    name: 'SideBar',
+    data() {
+        return {
+            sidebar,
+        }
+    },
+    methods: {
+        add(){
+            const name = prompt('Please input the name')
+            if (name) {
+                sidebar.make(name)
+            }
+        }
+    },
+}
+</script>
---
+++ src/states/sidebar.js
@@ -0,0 +1,39 @@
+class SideBar {
+    constructor() {
+        this.title = ''
+        this.item = null
+        this.manager = null
+    }
+
+    get first() {
+        if (this.manager) {
+            if (this.manager.list.length) {
+                return this.manager.list[0]
+            }
+        }
+        return null
+    }
+
+    make(name) {
+        const item = this.manager.make(name)
+        this.manager.add(item)
+    }
+
+    remove(item) {
+        this.manager.remove(item)
+        this.item = this.first
+    }
+
+    show(title, manager, item = null) {
+        this.title = title
+        this.manager = manager
+        if (item) {
+            this.item = item
+            return
+        }
+
+        this.item = this.first
+    }
+}
+
+export default new SideBar()
