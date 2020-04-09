38aa84d56647643e463ed0cf504610977d3492a7
GooGee <mou-gj@live.com>
2020-04-09 14:17:18 +0800

    + Template

--- src/router.js
+++ src/router.js
@@ -19,6 +19,11 @@ export default new Router({
             component: require('@/pages/Script').default,
         },
         {
+            path: '/template',
+            name: 'template',
+            component: require('@/pages/Template').default,
+        },
+        {
             path: '*',
             redirect: '/',
         },
