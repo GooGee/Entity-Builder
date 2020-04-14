b9e106a6db9ae35e8b9b167c7db15c35cd8ed3d1
GooGee <mou-gj@live.com>
2020-04-14 13:26:12 +0800

    - listenItemAdd in builder

--- src/states/builder.js
+++ src/states/builder.js
@@ -1,4 +1,3 @@
-import Entity from './entity.js'
 import storage from './storage'
 
 class Builder {
@@ -6,16 +5,6 @@ class Builder {
         this.project = null
         this.preset = null
         this.projectList = storage.getProjectList()
-        Entity.ee.on(Entity.EventEnum.AfterItemAdd, this.listenItemAdd)
-    }
-
-    listenItemAdd(sender, item) {
-        if (item.constructor.name === 'Entity') {
-            const table = item.FileManager.make('Migration')
-            item.FileManager.add(table)
-            const model = item.FileManager.make('Model')
-            item.FileManager.add(model)
-        }
     }
 
 }
