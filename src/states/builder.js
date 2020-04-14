c076160d756abd009c48c7fe6aa3fdc4dad2dd88
GooGee <mou-gj@live.com>
2020-04-14 10:30:32 +0800

    + Migration Model after Entity created

--- src/states/builder.js
+++ src/states/builder.js
@@ -1,3 +1,4 @@
+import Entity from './entity.js'
 import storage from './storage'
 
 class Builder {
@@ -5,7 +6,18 @@ class Builder {
         this.project = null
         this.preset = null
         this.projectList = storage.getProjectList()
+        Entity.ee.on(Entity.EventEnum.AfterItemAdd, this.listenItemAdd)
     }
+
+    listenItemAdd(sender, item) {
+        if (item.constructor.name === 'Entity') {
+            const table = item.FileManager.make('Migration')
+            item.FileManager.add(table)
+            const model = item.FileManager.make('Model')
+            item.FileManager.add(model)
+        }
+    }
+
 }
 
 export default new Builder()
