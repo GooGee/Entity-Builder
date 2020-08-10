6dcba2dcd3be9886497e0bfe77e9178fb9257e19
GooGee <mou-gj@live.com>
2020-08-11 07:30:04 +0800

    * no minify for class name

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,7 @@ module.exports = {
     lintOnSave: false,
     publicPath: '',
     chainWebpack: config => {
+        config.optimization.minimize(false)
         config.plugin('html').tap(args => {
             args[0].title = Title
             return args
