3235a43d6436761ae9a4a84706f40b3313703bad
GooGee <mou-gj@live.com>
2020-10-15 16:41:40 +0800

    + NameChangeListener.ts

---
+++ src/model/Event/NameChangeListener.ts
@@ -0,0 +1,42 @@
+import { EventEmitter } from 'events'
+import StrictEventEmitter from 'strict-event-emitter-types'
+import UniqueItem from '../Base/UniqueItem'
+
+enum EventEnum {
+    BeforeNameChange = 'BeforeNameChange',
+    AfterNameChange = 'AfterNameChange',
+}
+
+interface CallBack<T> {
+    (sender: T, name: string, old: string): void
+}
+
+interface Event<T> {
+    [EventEnum.BeforeNameChange]: CallBack<T>
+    [EventEnum.AfterNameChange]: CallBack<T>
+}
+
+class NameChangeListener<T> {
+    readonly ee: StrictEventEmitter<EventEmitter, Event<T>> = new EventEmitter()
+
+    emitAfterNameChange(sender: T, name: string, old: string) {
+        this.ee.emit(EventEnum.AfterNameChange, sender, name, old)
+    }
+
+    emitBeforeNameChange(sender: T, name: string, old: string) {
+        this.ee.emit(EventEnum.AfterNameChange, sender, name, old)
+    }
+
+    onAfterNameChange(callback: CallBack<T>) {
+        this.ee.on(EventEnum.AfterNameChange, callback)
+    }
+
+    onBeforeNameChange(callback: CallBack<T>) {
+        this.ee.on(EventEnum.BeforeNameChange, callback)
+    }
+}
+
+const listener = new NameChangeListener<UniqueItem>()
+listener.ee.setMaxListeners(111222333)
+
+export default listener
