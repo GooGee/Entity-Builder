7932ef2a6a8a07651a37a1d516c21506ced0dd10
GooGee <mou-gj@live.com>
2020-10-16 10:06:50 +0800

    + Top.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div class="container-fluid">
+            <router-view></router-view>
+        </div>
+        <Top></Top>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Top from './view/Top.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        Top,
+    },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/view/Top.vue
@@ -0,0 +1,18 @@
+<template>
+    <div style="position: fixed; right: 11%; bottom: 3%;">
+        <button class="btn btn-outline-primary rounded-pill" @click="window.scrollTo(0, 0)">
+            Top
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Top',
+    data() {
+        return {
+            window: window,
+        }
+    },
+}
+</script>
