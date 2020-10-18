b7b5394393ea18e281a94d234dbfca8f76d351f2
GooGee <mou-gj@live.com>
2020-10-18 10:15:26 +0800

    + Script.vue

---
+++ src/view/schema/Script.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h3 class="mtb11 gray">Script</h3>
+        <b-form-textarea v-model="item.script" rows="22" spellcheck="false"></b-form-textarea>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Script',
+    props: {
+        item: {
+            type: Object,
+            required: true,
+        },
+    },
+}
+</script>
