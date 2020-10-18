619d57ee24ebc3de59ebe1ad2c1909581a800bff
GooGee <mou-gj@live.com>
2020-10-18 10:15:34 +0800

    + Template.vue

---
+++ src/view/schema/Template.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h3 class="mtb11 gray">Template</h3>
+        <b-form-textarea v-model="item.template" rows="22" spellcheck="false"></b-form-textarea>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Template',
+    props: {
+        item: {
+            type: Object,
+            required: true,
+        },
+    },
+}
+</script>
