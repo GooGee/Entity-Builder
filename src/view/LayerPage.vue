9ce39a2ddd5cb07875f403ddcc99b471bfa465f3
GooGee <mou-gj@live.com>
2020-10-18 10:15:44 +0800

    * LayerPage.vue

--- src/view/LayerPage.vue
+++ src/view/LayerPage.vue
@@ -4,80 +4,27 @@
             <SideBar :sidebar="sss.sidebar" title="Layer"></SideBar>
         </div>
 
-        <div class="col-9">
-            <table v-if="sss.sidebar.item" class="table b-table b-table-caption-top">
-                <caption>
-                    <h2 v-if="sss.sidebar.item.original">{{ sss.sidebar.item.name }}</h2>
-                    <b-button-group v-else>
-                        <DeleteButton :manager="sss.sidebar.manager" :item="sss.sidebar.item"></DeleteButton>
-                        <ChangeButton :item="sss.sidebar.item" name="name"></ChangeButton>
-                    </b-button-group>
-                </caption>
-                <thead>
-                    <tr>
-                        <th class="text-right" style="width: 111px;">name</th>
-                        <th>value</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td class="text-right">single</td>
-                        <td>
-                            <b-form-checkbox v-model="sss.sidebar.item.single" switch></b-form-checkbox>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="text-right">description</td>
-                        <td>
-                            <b-form-input v-model="sss.sidebar.item.description"></b-form-input>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="text-right">class name</td>
-                        <td>
-                            <b-form-input v-model="sss.sidebar.item.classPattern"></b-form-input>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="text-right">file name</td>
-                        <td>
-                            <b-form-input v-model="sss.sidebar.item.filePattern"></b-form-input>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="text-right">namespace</td>
-                        <td>
-                            <b-form-input v-model="sss.sidebar.item.nsPattern"></b-form-input>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="text-right">path</td>
-                        <td>
-                            <b-form-input v-model="sss.sidebar.item.path"></b-form-input>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="text-right">template</td>
-                        <td>
-                            <b-form-textarea
-                                v-model="sss.sidebar.item.template"
-                                rows="11"
-                                spellcheck="false"
-                            ></b-form-textarea>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="text-right">script</td>
-                        <td>
-                            <b-form-textarea
-                                v-model="sss.sidebar.item.script"
-                                rows="11"
-                                spellcheck="false"
-                            ></b-form-textarea>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+        <div v-if="sss.sidebar.item" class="col-9">
+            <p>
+                <b-button-group>
+                    <DeleteButton
+                        :manager="sss.sidebar.manager"
+                        :item="sss.sidebar.item"
+                        @deleted="sss.sidebar.item = null"
+                    ></DeleteButton>
+                    <ChangeButton :item="sss.sidebar.item" name="name"></ChangeButton>
+                </b-button-group>
+            </p>
+
+            <b-nav tabs>
+                <b-nav-item @click="tab = 'Property'" :active="tab === 'Property'"> Property </b-nav-item>
+                <b-nav-item @click="tab = 'Script'" :active="tab === 'Script'"> Script </b-nav-item>
+                <b-nav-item @click="tab = 'Template'" :active="tab === 'Template'"> Template </b-nav-item>
+            </b-nav>
+
+            <LayerProperty v-if="tab === 'Property'" :item="sss.sidebar.item"></LayerProperty>
+            <Script v-if="tab === 'Script'" :item="sss.sidebar.item"></Script>
+            <Template v-if="tab === 'Template'" :item="sss.sidebar.item"></Template>
         </div>
     </div>
 </template>
@@ -86,6 +33,9 @@
 import ChangeButton from './button/ChangeButton.vue'
 import DeleteButton from './button/DeleteButton.vue'
 import SideBar from './part/SideBar.vue'
+import LayerProperty from './schema/LayerProperty.vue'
+import Script from './schema/Script.vue'
+import Template from './schema/Template.vue'
 import sss from '../state.js'
 
 export default {
@@ -94,10 +44,14 @@ export default {
         ChangeButton,
         DeleteButton,
         SideBar,
+        LayerProperty,
+        Script,
+        Template,
     },
     data() {
         return {
             sss,
+            tab: 'Property',
         }
     },
     created() {
