4fbb6ce8e64f7774b5f15c99eb5e66f0859d7bc8
GooGee <mou-gj@live.com>
2020-10-18 10:16:22 +0800

    + ChangeButton.vue

---
+++ src/view/button/ChangeButton.vue
@@ -0,0 +1,35 @@
+<template>
+    <b-button @click="change" variant="outline-primary"> {{ item[name] }} </b-button>
+</template>
+
+<script>
+export default {
+    name: 'ChangeButton',
+    props: {
+        item: {
+            type: Object,
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+    },
+    methods: {
+        change() {
+            const value = prompt(`Please input the ${this.name}`, this.item[this.name])
+            if (value) {
+                try {
+                    this.item[this.name] = value
+                } catch (error) {
+                    this.$bvToast.toast(error.message, {
+                        title: 'i',
+                        variant: 'danger',
+                        solid: true,
+                    })
+                }
+            }
+        },
+    },
+}
+</script>
