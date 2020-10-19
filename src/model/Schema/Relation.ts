8da4daccf4ad93b27ea6985d73edacff2ef2d9ef
GooGee <mou-gj@live.com>
2020-10-19 12:13:11 +0800

    + Relation.ts

---
+++ src/model/Schema/Relation.ts
@@ -0,0 +1,32 @@
+import UniqueItem from '../Base/UniqueItem'
+import UniqueList from '../Base/UniqueList'
+import Entity from './Entity'
+import { LayerEnum } from './Layer'
+import Project from './Project'
+
+export default class Relation extends UniqueItem {
+    type: string
+    parameter: string = ''
+
+    constructor(name: string, type: string = 'belongsTo') {
+        super(name)
+        this.type = type
+    }
+}
+
+export class RelationManager extends UniqueList<Relation> {
+    constructor() {
+        super(Relation)
+    }
+
+    link(entity: Entity, project: Project) {
+        const relation = this.make(entity.snakeCase)
+        const model = project.layerManager.find(LayerEnum.Model)
+        if (model) {
+            const name = model.getClassName(project, entity)
+            relation.parameter = `${name}::class`
+            return relation
+        }
+        throw new Error('Model layer is missing!')
+    }
+}
