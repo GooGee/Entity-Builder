f80fba665bdc8ec19617905a7d72945d4942440f
GooGee <mou-gj@live.com>
2020-10-19 12:13:21 +0800

    + Field.ts

---
+++ src/model/Schema/Field.ts
@@ -0,0 +1,53 @@
+import UniqueItem from '../Base/UniqueItem'
+import UniqueList from '../Base/UniqueList'
+import { RuleManager } from './Rule'
+import Seed from './Seed'
+
+export default class Field extends UniqueItem {
+    allowNull: boolean = false
+    cast: string = ''
+    collation: string = ''
+    comment: string = ''
+    fillable: boolean = true
+    hidden: boolean = false
+    included: boolean = true
+    length: number | string = ''
+    type: string
+    useCurrent: boolean = false
+    unsigned: boolean = false
+    value: number | string = ''
+
+    readonly ruleManager = new RuleManager()
+    readonly seed = new Seed()
+
+    constructor(name: string, type: string = 'integer') {
+        super(name)
+        this.type = type
+    }
+
+    get isIncrement() {
+        return this.type.includes('ncrement')
+    }
+}
+
+export class FieldManager extends UniqueList<Field> {
+    constructor() {
+        super(Field)
+    }
+
+    make(name: string, type: string) {
+        const field = new Field(name, type)
+        return field
+    }
+
+    get hasIncrement() {
+        if (this.incrementField) {
+            return true
+        }
+        return false
+    }
+
+    get incrementField() {
+        return this.list.find(field => field.isIncrement)
+    }
+}
