0a569c6426c2fdf999544eebc00f73acf49900f1
GooGee <mou-gj@live.com>
2020-10-19 17:54:29 +0800

    + Project.ts

---
+++ src/model/Schema/Project.ts
@@ -0,0 +1,36 @@
+import NameItem from '../Base/NameItem'
+import { EntityManager } from './Entity'
+import { LayerManager } from './Layer'
+import { PresetManager } from './Preset'
+
+const Version = 1
+
+export default class Project extends NameItem {
+    version = Version
+    ns = 'App'
+
+    readonly layerManager = new LayerManager()
+    readonly presetManager = new PresetManager()
+    /**
+     * must load after LayerManager
+     */
+    readonly entityManager = new EntityManager()
+
+    validationScript: string = ''
+
+    get fileName() {
+        return 'laravel-generator.json'
+    }
+
+    getEntity(name: string) {
+        return this.entityManager.find(name)
+    }
+
+    getLayer(name: string) {
+        return this.layerManager.find(name)
+    }
+
+    getPreset(name: string) {
+        return this.presetManager.find(name)
+    }
+}
