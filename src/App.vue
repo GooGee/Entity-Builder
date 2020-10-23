7bda93d3ab70ce4654b6cb7fe86ab79ada8ed3ef
GooGee <mou-gj@live.com>
2020-10-23 11:23:46 +0800

    -+ App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,48 @@
 <template>
-    <div id="app">
+    <div id="app" style="margin-bottom: 33px;">
+        <Bar :ready="sss.ready"></Bar>
         <div class="container-fluid">
             <router-view></router-view>
         </div>
+        <InputDialogue></InputDialogue>
+        <ListDialogue></ListDialogue>
         <Top></Top>
     </div>
 </template>
 
 <script>
-import Top from './view/Top.vue'
+import Bar from './view/part/Bar.vue'
+import InputDialogue from './view/part/InputDialogue.vue'
+import ListDialogue from './view/part/ListDialogue.vue'
+import Top from './view/part/Top.vue'
+import sss from './state.js'
 
 export default {
     name: 'App',
     components: {
+        Bar,
+        InputDialogue,
+        ListDialogue,
         Top,
+    },
+    data() {
+        return {
+            sss,
+        }
+    },
+    created() {
+        fetch('preset.json')
+            .then(response => response.json())
+            .then(json => {
+                sss.preset = json
+            })
+            .catch(error => {
+                this.$bvToast.toast(error.message, {
+                    title: 'i',
+                    variant: 'danger',
+                    solid: true,
+                })
+            })
     },
 }
 </script>
