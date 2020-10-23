c462fe5d8b6801a80138b55affe28d3dbbb343ee
GooGee <mou-gj@live.com>
2020-10-23 08:49:06 +0800

    + EntityProperty.vue

---
+++ src/view/schema/EntityProperty.vue
@@ -0,0 +1,45 @@
+<template>
+    <table class="table b-table b-table-caption-top">
+        <caption>
+            <h3>Property</h3>
+        </caption>
+        <thead>
+            <tr>
+                <th class="text-right" style="width: 111px;">name</th>
+                <th>value</th>
+            </tr>
+        </thead>
+        <tbody>
+            <slot></slot>
+            <tr>
+                <td class="text-right">tableName</td>
+                <td>
+                    <ChangeButton :item="item" name="tableName"></ChangeButton>
+                </td>
+            </tr>
+            <tr>
+                <td class="text-right">description</td>
+                <td>
+                    <b-form-textarea v-model="item.description" rows="11"></b-form-textarea>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+import ChangeButton from '../button/ChangeButton.vue'
+
+export default {
+    name: 'EntityProperty',
+    components: {
+        ChangeButton,
+    },
+    props: {
+        item: {
+            type: Object,
+            required: true,
+        },
+    },
+}
+</script>
