bc0c42842f03ccee94b7194fd57220febbc17b32
GooGee <mou-gj@live.com>
2020-10-25 09:07:41 +0800

    - load in Home.vue

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -4,7 +4,6 @@
             <img v-if="loading" alt="loading" src="@/assets/loading.svg" />
             <img v-else alt="logo" src="@/assets/logo.svg" style="width: 222px;" />
             <h1 class="m11">Code Generator</h1>
-            <b-button v-if="!loading" @click="load" variant="outline-success"> Reload </b-button>
         </div>
     </div>
 </template>
@@ -17,24 +16,6 @@ export default {
     computed: {
         loading() {
             return sss.project === null
-        },
-    },
-    methods: {
-        load() {
-            try {
-                sss.bridge.load()
-                this.$bvToast.toast('OK', {
-                    title: 'i',
-                    variant: 'success',
-                    solid: true,
-                })
-            } catch (error) {
-                this.$bvToast.toast(error.message, {
-                    title: 'i',
-                    variant: 'danger',
-                    solid: true,
-                })
-            }
         },
     },
 }
