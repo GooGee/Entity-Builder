17e7d8e1f1ea51656ccdade8bbc2e95b00f68105
GooGee <mou-gj@live.com>
2020-10-25 09:43:41 +0800

    + download in ProjectPage.vue

--- src/view/ProjectPage.vue
+++ src/view/ProjectPage.vue
@@ -3,13 +3,16 @@
         <div class="col-9 offset-3">
             <div style="height: 71px;">
                 <h1 class="inline mr11">Project</h1>
-                <b-button @click="save" variant="outline-success"> Save </b-button>
+                <b-button-group>
+                    <b-button @click="download" variant="outline-success"> Download </b-button>
+                    <b-button v-if="connected" @click="save" variant="outline-success"> Save </b-button>
+                </b-button-group>
             </div>
 
             <b-nav tabs>
                 <b-nav-item @click="tab = 'Property'" :active="tab === 'Property'"> Property </b-nav-item>
                 <b-nav-item @click="tab = 'Script'" :active="tab === 'Script'"> Script </b-nav-item>
-                <!-- <b-nav-item @click="tab = 'Schema'" :active="tab === 'Schema'"> DataBase </b-nav-item> -->
+                <b-nav-item @click="tab = 'Schema'" :active="tab === 'Schema'"> DataBase </b-nav-item>
             </b-nav>
 
             <ProjectProperty v-if="tab === 'Property'" :item="sss.project"></ProjectProperty>
@@ -25,6 +28,7 @@
 import ProjectProperty from './schema/ProjectProperty.vue'
 import Script from './schema/Script.vue'
 import TableList from './schema/TableList.vue'
+import { isConnected, download, save, getErrorMessage } from '../request.js'
 import sss from '../state.js'
 
 export default {
@@ -40,15 +44,17 @@ export default {
             tab: 'Property',
         }
     },
+    computed: {
+        connected() {
+            return isConnected()
+        },
+    },
     methods: {
-        save() {
+        download() {
             try {
-                sss.bridge.save(sss.project)
-                this.$bvToast.toast('OK', {
-                    title: 'i',
-                    variant: 'success',
-                    solid: true,
-                })
+                const name = sss.project.fileName
+                const text = JSON.stringify(sss.project)
+                download(name, text)
             } catch (error) {
                 this.$bvToast.toast(error.message, {
                     title: 'i',
@@ -56,6 +62,24 @@ export default {
                     solid: true,
                 })
             }
+        },
+        save() {
+            save(sss.project)
+                .then(response => {
+                    this.$bvToast.toast('Project saved', {
+                        title: 'OK',
+                        variant: 'success',
+                        solid: true,
+                    })
+                })
+                .catch(error => {
+                    const message = getErrorMessage(error)
+                    this.$bvToast.toast(message, {
+                        title: 'i',
+                        variant: 'danger',
+                        solid: true,
+                    })
+                })
         },
     },
 }
