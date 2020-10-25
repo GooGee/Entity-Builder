7daca0e209fd7a1a6cfb8e5e559df948c0dc15e6
GooGee <mou-gj@live.com>
2020-10-25 09:29:16 +0800

    + Title in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
+const Title = 'Entity-Builder'
+
+process.env.VUE_APP_TITLE = Title
+process.env.VUE_APP_VERSION = require('./package.json').version
+
 module.exports = {
+    lintOnSave: false,
+    publicPath: '',
     chainWebpack: config => {
+        config.optimization.minimize(false)
+        config.plugin('html').tap(args => {
+            args[0].title = Title
+            return args
+        })
+
         config.resolve.extensions.merge(['.ts']).end()
         config.module
             .rule('ts')
