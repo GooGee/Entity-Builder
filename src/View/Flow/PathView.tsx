8153f01d2096acb4e7b7092b0cd4a3ab4c199984
GooGee <mou-gj@live.com>
2024-02-12 15:44:43 +0800

    add FileButton to PathView.tsx

--- src/View/Flow/PathView.tsx
+++ src/View/Flow/PathView.tsx
@@ -1,7 +1,9 @@
 import { makePathCRUD } from "@/Database/makeCRUD"
+import useFilezzStore from "@/Store/useFilezzStore"
 import useFlowPageStore from "@/Store/useFlowPageStore"
 import useToastzzStore from "@/Store/useToastzzStore"
 import DeleteChangeButton from "@/View/Button/DeleteChangeButton"
+import FileButton from "@/View/Button/FileButton"
 import showInput from "@/View/Dialog/showInput"
 import PathDetail from "@/View/Oapi/PathDetail"
 
@@ -15,37 +17,56 @@ export default function PathView(property: Property) {
     const sFlowPageStore = useFlowPageStore()
     const sToastzzStore = useToastzzStore()
 
+    const file = useFilezzStore.getState().find(property.module.fileId)
+
     return (
         <PathDetail
             entity={property.entity}
             item={property.path}
             module={property.module}
         >
-            <DeleteChangeButton
-                name={property.path.name}
-                onChange={function () {
-                    showInput("Please input the path", property.path.name)
-                        .then((response) => {
-                            if (response.isConfirmed) {
-                                return makePathCRUD()
-                                    .update({
-                                        ...property.path,
-                                        name: response.value,
-                                    })
-                                    .then(sFlowPageStore.setPath)
-                            }
-                        })
-                        .catch(sToastzzStore.showError)
-                }}
-                onDelete={function (isConfirmed) {
-                    if (isConfirmed) {
-                        makePathCRUD()
-                            .delete(property.path.id)
-                            .then(() => sFlowPageStore.setPath())
+            <div>
+                <DeleteChangeButton
+                    name={property.path.name}
+                    onChange={function () {
+                        showInput("Please input the path", property.path.name)
+                            .then((response) => {
+                                if (response.isConfirmed) {
+                                    return makePathCRUD()
+                                        .update({
+                                            ...property.path,
+                                            name: response.value,
+                                        })
+                                        .then(sFlowPageStore.setPath)
+                                }
+                            })
                             .catch(sToastzzStore.showError)
-                    }
-                }}
-            ></DeleteChangeButton>
+                    }}
+                    onDelete={function (isConfirmed) {
+                        if (isConfirmed) {
+                            makePathCRUD()
+                                .delete(property.path.id)
+                                .then(() => sFlowPageStore.setPath())
+                                .catch(sToastzzStore.showError)
+                        }
+                    }}
+                ></DeleteChangeButton>
+
+                {file === undefined ? (
+                    <span className="text-danger ms-3">
+                        route file {property.module.fileId} not found
+                    </span>
+                ) : (
+                    <FileButton
+                        action={""}
+                        className="ms-3"
+                        file={file}
+                        fullName
+                        entity={property.entity}
+                        module={property.module}
+                    ></FileButton>
+                )}
+            </div>
         </PathDetail>
     )
 }
