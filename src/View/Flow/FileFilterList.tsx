47f78ef270e4913174764a07bf24309378054104
GooGee <mou-gj@live.com>
2024-12-19 09:18:41 +0800

    add FileFilterList.tsx

---
+++ src/View/Flow/FileFilterList.tsx
@@ -0,0 +1,103 @@
+import useFilezzStore from "@/Store/useFilezzStore"
+import { useEffect, useState } from "react"
+import ColorButtonGroup from "../Button/ColorButtonGroup"
+import ColorEnum from "@/Model/ColorEnum"
+import { makeModuleActionCRUD } from "@/Database/makeCRUD"
+import showInput from "../Dialog/showInput"
+import useFlowPageStore from "@/Store/useFlowPageStore"
+import useToastzzStore from "@/Store/useToastzzStore"
+
+interface Property {
+    directoryId: number
+    isTest: boolean
+    ma: LB.ModuleAction
+    entity: LB.Entity
+}
+
+export default function FileFilterList(property: Property) {
+    const suseFilezzStore = useFilezzStore()
+    const sFlowPageStore = useFlowPageStore()
+    const sToastzzStore = useToastzzStore()
+
+    const found = suseFilezzStore.itemzz.find(
+        (item) => item.name.startsWith(property.ma.name) && item.color !== ColorEnum.white,
+    )
+
+    const [Color, setColor] = useState<string>(found?.color ?? ColorEnum.white)
+    const [Text, setText] = useState("")
+
+    useEffect(() => {
+        setColor(found?.color ?? ColorEnum.white)
+    }, [property.ma])
+
+    return (
+        <>
+            <tr>
+                <td colSpan={2}>
+                    <input
+                        value={Text}
+                        onChange={function (event) {
+                            setText(event.target.value)
+                        }}
+                        className="form-control mb-1"
+                        placeholder="filter by name"
+                        type="text"
+                    />
+
+                    <ColorButtonGroup color={Color} setColor={setColor}></ColorButtonGroup>
+                </td>
+            </tr>
+
+            {suseFilezzStore.itemzz
+                .filter(function (item) {
+                    if (Text) {
+                        return item.name.includes(Text)
+                    }
+                    return item.color === Color
+                })
+                .map(function (item) {
+                    return (
+                        <tr key={item.id}>
+                            <td>{item.name}</td>
+                            <td>
+                                <button
+                                    className="btn btn-outline-primary"
+                                    type="button"
+                                    onClick={function () {
+                                        showInput("please input the fileNamePattern", item.fileNamePattern)
+                                            .then(function (result) {
+                                                if (result.isConfirmed) {
+                                                    if (result.value) {
+                                                        const file = {
+                                                            ...item,
+                                                            directoryId: property.directoryId,
+                                                            isExtra: true,
+                                                            fileNamePattern: result.value,
+                                                            id: item.id,
+                                                        } as LB.ModuleActionFile
+                                                        if (property.isTest) {
+                                                            file.nameSpacePattern = property.entity.name
+                                                        } else {
+                                                            file.nameSpacePattern = property.ma.name
+                                                        }
+                                                        return makeModuleActionCRUD()
+                                                            .update({
+                                                                ...property.ma,
+                                                                filezz: [...property.ma.filezz, file],
+                                                            })
+                                                            .then((item) => sFlowPageStore.setAction(item.name, item))
+                                                    }
+                                                }
+                                            })
+                                            .catch(sToastzzStore.showError)
+                                    }}
+                                >
+                                    +
+                                </button>
+                            </td>
+                        </tr>
+                    )
+                })}
+        </>
+    )
+}
