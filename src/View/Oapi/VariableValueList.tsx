650c72df9f8c4cf031cfb9f46fdc526d2bc484d9
GooGee <mou-gj@live.com>
2025-08-22 13:16:27 +0800

    add Example and getValue in VariableValueList.tsx

--- src/View/Oapi/VariableValueList.tsx
+++ src/View/Oapi/VariableValueList.tsx
@@ -2,6 +2,31 @@ import useToastzzStore from "@/Store/useToastzzStore"
 import showInput from "../Dialog/showInput"
 import useImportModalStore from "@/Store/useImportModalStore"
 
+const Example = `e.g.
+200
+401
+404
+500
+
+or
+
+    case Day = 'Day';
+    case Month = 'Month';
+    case Week = 'Week';
+`
+
+function getValue(text: string) {
+    const match = text.match(/=\s*(.+);.*$/)
+    if (match) {
+        return trimText(match[1])
+    }
+    return trimText(text)
+}
+
+function trimText(text: string) {
+    return text.trim().replace(/^['"]+|['"]+$/g, "")
+}
+
 interface Property {
     item: LB.Variable
     update(item: LB.Variable): void
@@ -46,7 +71,7 @@ export default function VariableValueList(property: Property) {
                     type="button"
                     onClick={function () {
                         const ss = useImportModalStore.getState()
-                        ss.openCB(1, "import values", "Please input a list of values, one per line", function (text) {
+                        ss.openCB(1, "import values", Example, function (text) {
                             const linezz = text.split(/\r?\n/)
                             if (linezz.length === 0) {
                                 ss.close()
@@ -54,7 +79,13 @@ export default function VariableValueList(property: Property) {
                             }
 
                             const set = new Set<string>(property.item.enum)
-                            linezz.forEach((item) => set.add(item.trim()))
+                            linezz.forEach((item) => {
+                                const value = getValue(item)
+                                if (value === "") {
+                                    return
+                                }
+                                set.add(value)
+                            })
                             const zz = Array.from(set).sort()
                             property.update({
                                 ...property.item,
